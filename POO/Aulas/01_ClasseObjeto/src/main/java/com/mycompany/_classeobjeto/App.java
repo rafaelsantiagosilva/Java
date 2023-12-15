44732f8097dc1b2ab4f6327200c489c11261fd60
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-15 18:14:49 -0300

    Update App.java

--- POO/Aulas/01_ClasseObjeto/src/main/java/com/mycompany/_classeobjeto/App.java
+++ POO/Aulas/01_ClasseObjeto/src/main/java/com/mycompany/_classeobjeto/App.java
@@ -18,37 +18,37 @@ public class App {
         canetaAzul.ponta = (float) 0.5;
         canetaAzul.tampada = true;
         
-        // Scanner
-        Scanner scan = new Scanner(System.in);
-        
-        // Opção de método e de continuar
-        int opc;
-        String continuar = "S";
-        
-        // Perguntando para o User o que ele deseja fazer com a caneta
-        do{
-            System.out.println("O que você deseja fazer?");
-            System.out.print("1) Tampar\n2) Destampar\n3) Rabiscar\n4) Ver status\nR:");
-            opc = scan.nextInt();
+        try (// Scanner
+        Scanner scan = new Scanner(System.in)) {
+            // Opção de método e de continuar
+            int opc;
+            String continuar = "S";
             
-            // As opções
-            switch(opc){
-                case 1 -> canetaAzul.tampar();
-                    
-                case 2 -> canetaAzul.destampar();
-                    
-                case 3 -> canetaAzul.rabiscar();
+            // Perguntando para o User o que ele deseja fazer com a caneta
+            do{
+                System.out.println("O que você deseja fazer?");
+                System.out.print("1) Tampar\n2) Destampar\n3) Rabiscar\n4) Ver status\nR:");
+                opc = scan.nextInt();
                 
-                case 4 -> canetaAzul.status();
+                // As opções
+                switch(opc){
+                    case 1 -> canetaAzul.tampar();
+                        
+                    case 2 -> canetaAzul.destampar();
+                        
+                    case 3 -> canetaAzul.rabiscar();
                     
-                default -> System.out.println("ERRO: Por favor, digite um valor válido!");
-            }
-            
-            // Opção de reuso
-            System.out.print("Deseja continuar? ");
-            System.out.print("[S/N] \nR: ");
-            continuar = scan.next();
-            
-        }while("S".equals(continuar) || "s".equals(continuar));
+                    case 4 -> canetaAzul.status();
+                        
+                    default -> System.out.println("ERRO: Por favor, digite um valor válido!");
+                }
+                
+                // Opção de reuso
+                System.out.print("Deseja continuar? ");
+                System.out.print("[S/N] \nR: ");
+                continuar = scan.next();
+                
+            }while("S".equals(continuar) || "s".equals(continuar));
+        }
     }
 }
