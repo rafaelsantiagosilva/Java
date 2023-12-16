461f708c4189eab3e3b4d2bf98eee0bfbb970f2d
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-16 19:38:32 -0300

    Aula 06) Objetos Compostos

---
+++ POO/Aulas/06_ObjetosCompostos/src/main/java/com/mycompany/AcoesLutador.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+
+public interface AcoesLutador {
+     public abstract void apresentar();
+     public abstract void status();
+     public abstract void ganharLuta();
+     public abstract void perderLuta();
+     public abstract void empatarLuta();
+}
---
+++ POO/Aulas/06_ObjetosCompostos/src/main/java/com/mycompany/Main.java
@@ -0,0 +1,19 @@
+package com.mycompany;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando um array de objetos
+        Lutador lutadores[] = new Lutador[6];
+
+        lutadores[0] = new Lutador("Pretty", "França", 1992, 1.75f, 68.9f, 11, 2, 1);
+        lutadores[1] = new Lutador("Putsscript", "Brasil", 1995, 1.68f, 57.8f, 14, 2, 3);
+        lutadores[2] = new Lutador("Snapshadow", "EUA", 1988, 1.65f, 80.9f, 12, 2, 1);
+        lutadores[3] = new Lutador("Dead Code", "Austrália", 1995, 1.93f, 81.6f, 13, 0, 2);
+        lutadores[4] = new Lutador("UF0Cobol", "Brasil", 1986, 1.70f, 119.3f, 5, 4, 3);
+        lutadores[5] = new Lutador("Nerdaart", "EUA", 1993, 1.81f, 105.7f, 12, 2, 4);
+
+        for (int i = 0; i < 6; i++) {
+            lutadores[i].status();
+        }
+    }
+}
